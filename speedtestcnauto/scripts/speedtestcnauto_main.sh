74c9ae405a1bff22b062665a0e40cdfd31f6792b
WengHeng <v1@v1v5.cn>
2022-02-11 16:13:50 +0800

    test 1.3

--- speedtestcnauto/scripts/speedtestcnauto_main.sh
+++ speedtestcnauto/scripts/speedtestcnauto_main.sh
@@ -98,14 +98,12 @@ self_upgrade(){
    # shellcheck disable=SC2154
    # shellcheck disable=SC2046
    if [ $(expr "$new_version" \> "$old_version") -eq 1 ];then
-
        tmpDir="/tmp/upload/speedtestcnauto_up/"
        mkdir -p $tmpDir
        echo_date "新版本:${new_version}已发布,开始更新..." >> $LOGFILE
        echo_date "下载资源新版本资源..." >> $LOGFILE
        versionfile=$(echo "${version_info}"|jq_speed .fileurl |sed 's/\"//g')
-       echo_date "${versionfile}" >> $LOGFILE
-       wget -O ${tmpDir}speedtestcnauto.tar.gz "${versionfile}"
+       wget --no-cache -O ${tmpDir}speedtestcnauto.tar.gz "${versionfile}"
        if [ -f "${tmpDir}speedtestcnauto.tar.gz" ];then
          # shellcheck disable=SC2129
          echo_date "新版本下载成功.." >> $LOGFILE
